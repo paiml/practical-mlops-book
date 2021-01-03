3a7f9bf9588777a91e8873716d0e6c68c1e99e1f
Alfredo Deza <alfredo@deza.pe>
2021-01-03 09:37:20 -0500

    use an entry_point for setup.py instead

--- chapter11/linter/setup.py
+++ chapter11/linter/setup.py
@@ -5,7 +5,10 @@ setup(
     description = 'lint csv files',
     packages = find_packages(),
     author = 'Alfredo Deza',
-    scripts = ['bin/csv-linter'],
+    entry_points="""
+    [console_scripts]
+    csv-linter=csv_linter:main
+    """,
     install_requires = ['click==7.1.2', 'pandas==1.2.0'],
     version = '0.0.1',
     url = 'https://github.com/paiml/practical-mlops-book',
