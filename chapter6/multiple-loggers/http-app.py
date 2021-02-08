c46e13283c3cdf44193d947862f249e1ff9222d5
Alfredo Deza <alfredo@deza.pe>
2021-02-08 08:13:40 -0500

    update http-app

--- chapter6/multiple-loggers/http-app.py
+++ chapter6/multiple-loggers/http-app.py
@@ -1,13 +1,15 @@
 import requests
 import logging
+
 logging.basicConfig()
 root_logger = logging.getLogger()
+
+# Sets logging level for every single app, the "parent" logger
+root_logger.setLevel(logging.DEBUG)
 
 # new logger for this script
 logger = logging.getLogger('http-app')
 
-# Sets logging level for every single app, the "parent" logger
-root_logger.setLevel(logging.DEBUG)
 
 logger.info("About to send a request to example.com")
 requests.get('http://example.com')
@@ -15,6 +17,6 @@ requests.get('http://example.com')
 # fine tune the urllib logger:
 urllib_logger = logging.getLogger('urllib3')
 urllib_logger.setLevel(logging.ERROR)
-
+#
 logger.info("About to send another request to example.com")
 requests.get('http://example.com')
