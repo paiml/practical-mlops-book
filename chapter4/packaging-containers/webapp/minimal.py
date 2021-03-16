db8f8483e1d8dcb5d1d5b8ebdbc5d9e94afb7e9d
Alfredo Deza <alfredo@deza.pe>
2021-03-16 17:30:20 -0400

    chapter4 container changes
    Signed-off-by: Alfredo Deza <alfredo@deza.pe>

--- chapter4/packaging-containers/webapp/minimal.py
+++ chapter4/packaging-containers/webapp/minimal.py
@@ -1,42 +1,22 @@
 from flask import Flask, request, jsonify
 import torch
 import numpy as np
-from transformers import RobertaTokenizer
+#from transformers import RobertaTokenizer
 import onnxruntime
 
 
 app = Flask(__name__)
 tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
-session = onnxruntime.InferenceSession("roberta-sequence-classification-9.onnx")
-
-
-def to_numpy(tensor):
-    return (
-        tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
-    )
-
-
-@app.route("/")
-def home():
-    return "<h3>RoBERTa Sentiment Analysis Prediction Container</h3>"
+session = onnxruntime.InferenceSession(
+    "roberta-sequence-classification-9.onnx"
+)
 
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    """
-    Input sample:
-
-        [ "Containers are good" ]
-
-    Output sample:
-
-        {"postive": True}
-    """
     input_ids = torch.tensor(
         tokenizer.encode(request.json[0], add_special_tokens=True)
-    ).unsqueeze(
-        0
-    )
+    ).unsqueeze(0)
 
     if input_ids.requires_grad:
         numpy_func = input_ids.detach().cpu().numpy()
